50f586e6ff3e862aa1c3858f38a3c941bf6ad11e
KozelMV <kozelmv@gmail.com>
2017-03-09 21:04:50 +0300

    Выполненное упражнение

--- (page_358)exercise_9/(page_358)exercise_9.cpp
+++ (page_358)exercise_9/(page_358)exercise_9.cpp
@@ -12,12 +12,20 @@ class safearray
 {
 private:
 	int arr[LIMIT];
+	int str, fin;
 public:
-
+	//конструктор с двум€ аргументами
+	safearray() : str(100), fin(175) 
+	{
+		if (str >= LIMIT)
+			str = str % LIMIT;
+		if (fin > LIMIT)
+			fin = fin % LIMIT;
+	}
 	// обратите внимание, что функци€ возвращает ссылку!
 	int& operator[](int n)
 	{
-		if (n < 0 || n >= LIMIT)
+		if (n < 0 || n > LIMIT)
 		{
 			std::cout << "\nќшибочный индекс!\n"; 
 			system("pause");  
@@ -25,19 +33,22 @@ public:
 		}
 		return arr[n];
 	}
+	int down() { return str; };
+	int top() { return fin; };
 };
 ///////////////////////////////////////////////////////////
 int main()
 {
 	setlocale(LC_ALL, "");
-	safearray sa1;
+	
 
+	safearray sa1;
 	// задаем значени€ элементов
-	for (int j = 0; j < LIMIT+2; j++)
+	for (int j = sa1.down(); j <= sa1.top(); j++)
 		sa1[j] = j * 10;    // используем функцию слева от знака =
 
 							// показываем элементы
-	for (int j = 0; j < LIMIT; j++)
+	for (int j = sa1.down(); j <= sa1.top(); j++)
 	{
 		int temp = sa1[j];  // используем функцию справа от знака =
 		std::cout << "Ёлемент " << j << " равен " << temp << std::endl;
