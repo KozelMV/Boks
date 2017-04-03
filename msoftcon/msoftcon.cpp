e41eae7f7ea678e34e608cb09d57f534d35b7750
KozelMV <kozelmv@gmail.com>
2017-04-03 22:49:55 +0300

    Consol Graphikc my comments ru_UTF-8

--- msoftcon/msoftcon.cpp
+++ msoftcon/msoftcon.cpp
@@ -1,152 +1,153 @@
-//msoftcon.cpp
-//подпрограммы доступа к консольным функциям Windows 
-
-//компилятор должен знать, где искать этот файл
-//в MCV++, /Tools/Options/Directories/Include/type путь
 
 #include "msoftcon.h"
-HANDLE hConsole;         //поддержка консольного режима 
-char fill_char;          //символ заполнения
-//---------------------------------------------------------
+
+
+HANDLE hConsole; //Дескрипторный тип данных
+unsigned char fill_char;          //Символ для заливки
+//--------------------------------------------------------------
 void init_graphics()
    {
-   COORD console_size = {80, 25};
-   //открыть канал ввода/вывода на консоль
-   hConsole = CreateFile("CONOUT$", GENERIC_WRITE | GENERIC_READ,
+   COORD console_size = {80, 25}; //Размер консоли
+
+   /*Функция CreateFile() открывает каталог
+   -значение CONOUT$, чтобы определить консольный вывод данных
+   -флаги GENERIC_WRITE | GENERIC_READ Открывают консоль для вывода/ввода данных.
+   -флаги  FILE_SHARE_READ | FILE_SHARE_WRITE Разрешения для чтения и для записи
+   -флаг OPEN_EXISTING открывает консоль
+   -флаг FILE_ATTRIBUTE_NORMAL использование стандартных атрибутов*/
+   
+   hConsole = CreateFile(L"CONOUT$", GENERIC_WRITE | GENERIC_READ,
                    FILE_SHARE_READ | FILE_SHARE_WRITE,
                    0L, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0L);
-   //установить размер экрана 80x25 
-   SetConsoleScreenBufferSize(hConsole, console_size);
-   //текст белым по черному 
-   SetConsoleTextAttribute( hConsole, (WORD)((0 << 4) | 15) );
 
-   fill_char = '\xDB';  //заполнение по умолчанию
-   clear_screen();
+   SetConsoleScreenBufferSize(hConsole, console_size); //Устанавливаем размер экранного буфера консоли
+   
+   SetConsoleTextAttribute( hConsole, (WORD)((0 << 4) | 15) ); //Устанавливает атрибуты символов(цвет текста и фона) по умалчанию
+ 
+   fill_char = '\xDB';  //Символ заполнения по умалчанию
+   clear_screen(); //Очистка экрана
    }
-//---------------------------------------------------------
-void set_color(color foreground, color background)
+//--------------------------------------------------------------
+void set_color(color foreground, color background) //Окрас элемента и заднего фона
    {
-   SetConsoleTextAttribute( hConsole, 
-                        (WORD)((background << 4) | foreground) );
-   }  //конец setcolor()
-
-/* 0  Черный          8  Темно-серый
-   1  Темно-синий     9  Синий
-   2  Темно-зеленый   10 Зеленый
-   3  Темно-голубой   11 Алый
-   4  Темно-красный   12 красный
-   5  Темно-алый      13 алый
-   6  Коричневый      14 Желтый
-   7  Светло-серый    15 Белый
+   SetConsoleTextAttribute( hConsole,
+                        (WORD)((background << 4) | foreground) ); //Устанавливает атрибуты символов(цвет текста и фона) по значению
+   }  //end setcolor()
+ 
+/* 0  Black          8  Dark gray
+   1  Dark blue      9  Blue
+   2  Dark green     10 Green
+   3  Dark cyan      11 Cyan
+   4  Dark red       12 Red
+   5  Dark magenta   13 Magenta
+   6  Brown          14 Yellow
+   7  Light gray     15 White
 */
-//---------------------------------------------------------
-void set_cursor_pos(int x, int y)
+//--------------------------------------------------------------
+void set_cursor_pos(int x, int y) //Устанавливаем курсор в позицию
    {
-   COORD cursor_pos;            //Начало в верхнем левом 
-   cursor_pos.X = x - 1;        //Windows начинает с (0, 0)
-   cursor_pos.Y = y - 1;        //мы начнем с (1, 1)
-   SetConsoleCursorPosition(hConsole, cursor_pos);
+   COORD cursor_pos;              //Определяем координаты в буфере консоли
+   cursor_pos.X = x - 1;          //Windows starts at (0, 0)
+   cursor_pos.Y = y - 1;          //we start at (1, 1)
+   SetConsoleCursorPosition(hConsole, cursor_pos); //Устанавливаем курсор в точку заданную координатами
    }
-//---------------------------------------------------------
-void clear_screen()
+//--------------------------------------------------------------
+void clear_screen() //Очищаем экран
    {
-   set_cursor_pos(1, 25);
-   for(int j=0; j<25; j++)
-      putch('\n');
-   set_cursor_pos(1, 1);
+   set_cursor_pos(1, 25);  //.Устанавливаем курсор в заданные координаты
+   for(int j=0; j<25; j++) //.
+      _putch('\n'); 	   //Спускаемся на 24 строки вниз
+   set_cursor_pos(1, 1);   //Устанавливаем курсор в заданные координаты
    }
-//---------------------------------------------------------
-void wait(int milliseconds)
+//--------------------------------------------------------------
+void wait(int milliseconds) //Ожидаем
    {
-   Sleep(milliseconds);
+   Sleep(milliseconds); //Функция останавливает выполнение программы на заданное время
    }
-//---------------------------------------------------------
-void clear_line()                 //очистка до конца строки
-   {                              //80 пробелов
-   //.....1234567890123456789012345678901234567890
-   //.....0........1.........2.........3.........4 
-   cputs("                                        ");
-   cputs("                                        ");
+//--------------------------------------------------------------
+void clear_line()                    //Очищаем до конца строки
+   {                                 //80 пробелов
+   //......1234567890123456789012345678901234567890
+   //......0........1.........2.........3.........4
+   _cputs("                                        ");//.
+   _cputs("                                        ");//.Выводим строку
    }
-//---------------------------------------------------------
-void draw_rectangle(int left, int top, int right, int bottom) 
-   {
-   char temp[80];
-   int width = right - left + 1;
+//--------------------------------------------------------------
+void draw_rectangle(int left, int top, int right, int bottom) //Рисуем прямоугольник
+{
+	char temp[80]; //Строковый массив для фигуры(При превышение размера ошибка программы)
+	int width = right - left + 1; //Возвращаем ширину к заданым параметрам и + 1
+	for (int j = 0; j < width; j++)   //.
+		temp[j] = fill_char;		  //.Печатаем строку квадрата на заданную длину
+	temp[width] = 0;  //Завершаем строку нулем
 
-   for(int j=0; j<width; j++)      //строка квадратов
-      temp[j] = fill_char;   
-   temp[j] = 0;                    //null
-
-   for(int y=top; y<=bottom; y++)  //строковый стек 
-      {
-      set_cursor_pos(left, y);
-      cputs(temp);
-      }
-   }
-//---------------------------------------------------------
-void draw_circle(int xC, int yC, int radius)
+	for (int y = top; y <= bottom; y++)  //Печатаем ширину по заданным пораметрам
+	{
+		set_cursor_pos(left, y);//Устанавливаем курсор в заданные координаты
+		_cputs(temp); //Выводим созданную строку квадрата
+	}
+}
+//--------------------------------------------------------------
+void draw_circle(int xC, int yC, int radius/*=5*/) //Рисуем круг (для коректного отображения(есле !r=1) -> xC>=r*2+1, yC>=r+1, r>1)
    {
+
    double theta, increment, xF, pi=3.14159;
    int x, xN, yN;
-
-   increment = 0.8 / static_cast<double>(radius);
-   for(theta=0;theta<=pi/2;theta+=increment)//1/4 
-                                            //окружности
+ 
+   increment = 0.8 / static_cast<double>(radius); //Получем значение инкримента =0,16
+   for(theta=0; theta <= pi/2 /*1,570795*/; theta+=increment/*+=0,16 на каждой итерации*/)  // Четверть окружности
       {
-      xF = radius * cos(theta);  
-      xN = static_cast<int>(xF * 2 / 1); //пикселы не
-                                         //квадратные :-(
-      yN = static_cast<int>(radius * sin(theta) + 0.5);
-      x = xC-xN;
-      while(x <= xC+xN) //заполнить две горизонтальные линии
-         {              //по одной на каждую ? окружности
-         set_cursor_pos(x, yC-yN); putch(fill_char); //верх
-         set_cursor_pos(x++,yC+yN); putch(fill_char); //низ
+      xF = radius * cos(theta); //=5, 4.93614, 4.74618, 4.010487, 3.48353, 2.17841, 0.6521198
+      xN = static_cast<int>(xF * 2 / 1); //=10, 9, 8, 6, 4, 5, 2 
+      yN = static_cast<int>(radius * sin(theta) + 0.5); //=0, 1, 2, 3, 4, 5, 5
+      x = xC-xN; //=30, 31, 31, 32, 34, 36, 39, 8 Находим координату X для начала строки
+      while(x <= xC+xN)  //Отрисовываем две строки
+         {                       
+         set_cursor_pos(x,   yC-yN); _putch(fill_char);  //Верх (30,12)(31,11)(31,10)(32,9) (34,8) (36,7) (39,7)
+         set_cursor_pos(x++, yC+yN); _putch(fill_char);  //Низ  (31,12)(32,13)(32,14)(33,15)(35,16)(37,17)(40,17)
          }
-      }  //конец for
+      }  //end for
    }
-//---------------------------------------------------------
-void draw_line(int x1, int y1, int x2, int y2)
+//--------------------------------------------------------------
+void draw_line(int x1, int y1, int x2, int y2) //Рисуем линию
    {
-
    int w, z, t, w1, w2, z1, z2;
-   double xDelta=x1-x2, yDelta=y1-y2, slope;
-   bool isMoreHoriz;
-
-   if( fabs(xDelta) > fabs(yDelta) ) //еще горизонтальная
+   double xDelta=x1-x2 /*Длинна*/, yDelta=y1-y2 /*Ширина*/, slope/*угал*/;
+   bool isMoreHoriz; //Горизонтальная 1 или вертикальная 0
+ 
+   if( fabs(xDelta) > fabs(yDelta)/*Сравниваем числа по модолю*/ ) //Горизонтальная линия
       {
       isMoreHoriz = true;
-      slope = yDelta / xDelta;
-      w1=x1; z1=y1; w2=x2, z2=y2;    //w=x, z=y 
+      slope = yDelta / xDelta;  // наклон
+      w1=x1; z1=y1; w2=x2, z2=y2;    
       }
-   else                              //еще вертикальная
+   else                              //Вертикалная линия
       {
       isMoreHoriz = false;
-      slope = xDelta / yDelta;
-      w1=y1; z1=x1; w2=y2, z2=x2;    //w=y, z=x
+      slope = xDelta / yDelta; // наклон
+      w1=y1; z1=x1; w2=y2, z2=x2;    
       }
-
-   if(w1 > w2)                       //если за w
+ 
+   if(w1 > w2)           //Если начальные координаты больше конечных по Х
       {
-      t=w1; w1=w2; w2=t;             //   заменить (w1,z1)
-      t=z1; z1=z2; z2=t;             //   на (w2,z2)
+      t=w1; w1=w2; w2=t;             //.меняем значения (w1,w2) (z1,z2) местами
+      t=z1; z1=z2; z2=t;             //. 
       }
-   for(w=w1; w<=w2; w++)            
+   for(w=w1/*5*/; w<=/*10*/w2; w++) //Запускаем цикл по отрисовке           
       {
-      z = static_cast<int>(z1 + slope * (w-w1));
-      if( !(w==80 && z==25) )  //убрать прокрутку на 80,25
+      z = static_cast<int>(z1 + slope * (w-w1)); //Получаем координату по X с учетом отступа
+      if( !(w==80 && z==25) )        //avoid scroll at 80,25
          {
          if(isMoreHoriz)
-            set_cursor_pos(w, z);
+            set_cursor_pos(w, z); //Устанавливаем курсор на позицию
          else
-            set_cursor_pos(z, w);
-         putch(fill_char);
+            set_cursor_pos(z, w); //Устанавливаем курсор на позицию
+         _putch(fill_char); //Отрисовываем символ
          }
       }
    }
-//---------------------------------------------------------
-void draw_pyramid(int x1, int y1, int height)
+//--------------------------------------------------------------
+void draw_pyramid(int x1, int y1, int height) //Рисуем треугольник
    {
    int x, y;
    for(y=y1; y<y1+height; y++)
@@ -154,14 +155,15 @@ void draw_pyramid(int x1, int y1, int height)
       int incr = y - y1;
       for(x=x1-incr; x<=x1+incr; x++)
          {
-         set_cursor_pos(x, y);
-         putch(fill_char);
+         set_cursor_pos(x, y); //Устанавливаем курсор на позицию
+         _putch(fill_char);	   //Отрисовываем символ
          }
       }
    }
-//---------------------------------------------------------
-void set_fill_style(fstyle fs)
+//--------------------------------------------------------------
+void set_fill_style(fstyle fs) //Тип заполнения(заливка)
    {
+	
    switch(fs)
       {
       case SOLID_FILL:  fill_char = '\xDB'; break;
@@ -172,6 +174,4 @@ void set_fill_style(fstyle fs)
       case O_FILL:      fill_char = 'O';    break;
       }
    }
-//---------------------------------------------------------
-
-
+//--------------------------------------------------------------
--- msoftcon/msoftcon.h
+++ msoftcon/msoftcon.h
@@ -2,33 +2,35 @@
 //ќбъ¤влени¤ функций консольной графики от Lafore
 //используютс¤ консольные функции Windows 
 
-#ifndef _INC_WCONSOLE    //этот файл не должен включатьс¤
-#define _INC_WCONSOLE    //дважды в тот же исходный файл
-
-#include <windows.h>     //дл¤ консольных функций Windows
+#ifndef _INC_WCONSOLE    //ƒеректива провер¤ет включение файла аналогично оператору if(!define)
+#define _INC_WCONSOLE    //»м¤ _INC_WCONSOLE выбрано произвольно 
+						 //»спользуютс¤ чтобы исключить повторное включение содержимого заголовочных файлов т.к. 
+						 //include втупую подставл¤ет всЄ содержимое по месту объ¤влени¤. 
+#include <windows.h>     //дл¤ консольных функций Windows Sleep()
 #include <conio.h>       //дл¤ kbhit(), getche()
-#include <math.h>        //дл¤ sin, cos
+#include <math.h>        //дл¤ sin, cos fabs()
 
-enum fstyle { SOLID_FILL, X_FILL,      O_FILL, 
-              LIGHT_FILL, MEDIUM_FILL, DARK_FILL };
+enum fstyle { SOLID_FILL, X_FILL, O_FILL, //—писок возможных заполнений символами, заливок
+	LIGHT_FILL, MEDIUM_FILL, DARK_FILL }; 
 
-enum color {
+enum color { //—писок цветавой палитры
    cBLACK=0,     cDARK_BLUE=1,  cDARK_GREEN=2, DARK_CYAN=3, 
    cDARK_RED=4,  cDARK_MAGENTA=5, cBROWN=6,  cLIGHT_GRAY=7,
    cDARK_GRAY=8, cBLUE=9,         cGREEN=10,     cCYAN=11, 
    cRED=12,      cMAGENTA=13,     cYELLOW=14,    cWHITE=15 };
 //---------------------------------------------------------
 void init_graphics();
-void set_color(color fg, color bg = cBLACK);
-void set_cursor_pos(int x, int y);
-void clear_screen();
-void wait(int milliseconds);
-void clear_line();
-void draw_rectangle(int left, int top, int right, int bottom);                    
-void draw_circle(int x, int y, int rad);
-void draw_line(int x1, int y1, int x2, int y2);
-void draw_pyramid(int x1, int y1, int height);
-void set_fill_style(fstyle);
+void set_color(color fg, color bg = cBLACK); //‘ункци¤ цвета(цвет элемента, цвет заднего фона)
+void set_cursor_pos(int x, int y); //‘ункци¤ установки курсора в заданные координаты
+void clear_screen(); //‘ункци¤ очистки экрана
+void wait(int milliseconds); //‘ункци¤ останавливает выполнение программы на заданное врем¤
+void clear_line(); //ќчищаем строку
+void draw_rectangle(int left, int top, int right, int bottom); //–исуем пр¤моугольник по заданным пораметрам                    
+void draw_circle(int x, int y, int rad); //–исуем круг
+void draw_line(int x1, int y1, int x2, int y2); //–исуем линию
+void draw_pyramid(int x1, int y1, int height); //–исуем треугольник
+void set_fill_style(fstyle); //‘ункци¤ заливки
+
 #endif /* _INC_WCONSOLE */
 
 
